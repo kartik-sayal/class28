3a3e4f78f932dcbe00f2ec698aaf31103532f92c
kartik-sayal <sayalkartik1537@gmail.com>
2020-12-29 13:46:54 +0530

    added a slingshot

--- Chain.js
+++ Chain.js
@@ -1,20 +1,23 @@
 class Chain{
-    constructor(bodyA, bodyB){
+    constructor(bodyA, pointB){
         var options = {
             bodyA: bodyA,
-            bodyB: bodyB,
+            pointB: pointB,
             stiffness: 0.04,
             length: 10
         }
         this.chain = Constraint.create(options);
         World.add(world, this.chain);
+        this.pointB = pointB;
     }
 
     display(){
+        if(this.chain.bodyA){
         var pointA = this.chain.bodyA.position;
-        var pointB = this.chain.bodyB.position;
+        var pointB = this.pointB;
         strokeWeight(4);
         line(pointA.x, pointA.y, pointB.x, pointB.y);
     }
+}
     
 }
